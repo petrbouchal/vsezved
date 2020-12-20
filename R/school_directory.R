0f9a7f265bbaf377be6764dc7407597fa02e9afa
Petr Bouchal <pbouchal@gmail.com>
2020-12-20 18:24:25 +0100

    switch to new rvest + rvest's form filling + break out core fn

--- R/school_directory.R
+++ R/school_directory.R
@@ -19,7 +19,97 @@ sk_get_search_fields <- function(search_form = NULL) {
 
 }
 
+sk_get_directory_responses <- function(tables, ...) {
+  available_tables <- c("addresses", "schools", "locations", "specialisations")
 
+  if(any(!tables %in% available_tables)) {
+    wrong_tables <- tables[!tables %in% available_tables]
+    usethis::ui_stop("Table(s) {usethis::ui_value(wrong_tables)} not available")
+  }
+
+  if(is.null(tables)) {
+    tabs <- c("addresses")
+    usethis::ui_info(c("No {usethis::ui_field('tables')} param set.",
+                       "Using {usethis::ui_value('addresses')}."))
+  } else {
+    tabs <- tables
+  }
+
+  search_page <- sk_get_search_page()
+  search_form <- sk_get_search_form(search_page)
+
+  # the form does not indicate defaults (even though it seems like it
+  # in the browser), so we have to set them manually - otherwise no
+  # results are returned.
+
+  search_form_with_defaults <- rvest::form_set(search_form,
+                                               uzemi = "NIC",
+                                               zrizovatel = "NIC",
+                                               organ = "NIC",
+                                               typ = "NIC",
+                                               jazs = "NIC",
+                                               delka = "NIC",
+                                               forma = "NIC",
+                                               jazob = "NIC",
+                                               skupobor = "NIC",
+                                               kmobor = "NIC",
+                                               obor = "NIC")
+
+  if(!missing(...)) {
+    search_form_with_userinput <- rvest::form_set(search_form_with_defaults,
+                                                  ...)
+    search_form_filled <- search_form_with_userinput
+  } else {
+    search_form_filled <- search_form_with_defaults
+  }
+
+  results_page <- rvest::form_submit(search_form_filled, search_page, "XX")
+
+  results_forms <- results_page %>% xml2::read_html() %>%
+    rvest::html_nodes("form") %>%
+    rvest::html_form()
+
+  export_page <- rvest::form_submit(results_forms[[2]], results_page, "EX")
+
+  export_forms <- rvest::html_form(export_page)
+
+  responses <- list()
+  if("addresses" %in% tabs) {
+    responses <- append(responses, list(rvest::form_submit(export_forms[[1]], export_page, "EX")))
+    names(responses) <- c(names(responses), "addresses")
+  }
+  if("schools" %in% tabs) {
+    responses <- append(responses, list(rvest::form_submit(export_forms[[2]], export_page, "EXX")))
+    names(responses) <- c(names(responses), "schools")
+  }
+  if("locations" %in% tabs) {
+    responses <- append(responses, list(rvest::form_submit(export_forms[[3]], export_page, "EXM")))
+    names(responses) <- c(names(responses), "locations")
+  }
+  if("specialisations" %in% tabs) {
+    responses <- append(responses, list(rvest::form_submit(export_forms[[4]], export_page, "EXO")))
+    names(responses) <- c(names(responses), "specialisations")
+  }
+
+  return(responses)
+}
+
+response_to_quasixls <- function(response, tempfile = TRUE) {
+
+  if(tempfile) {
+    path <- tempfile(fileext = ".xls")
+  } else {
+    attachment_string <- response$response$headers$`content-disposition`
+    path <- regmatches(attachment_string,
+                       regexpr("(?<=filename\\=)(\\w*.xls$)",
+                               attachment_string, perl = T))
+  }
+
+  r3_c_bin <- httr::content(response$response)
+  writeBin(r3_c_bin, path)
+
+  return(path)
+}
 
 #' Get school directory
 #'
@@ -32,7 +122,7 @@ sk_get_search_fields <- function(search_form = NULL) {
 #' register.
 #'
 #' @param tables a character vector of tables to retrieve. See Details.
-#' @param fields a named character vector of search fields. Use `sk_get_search_fields()`
+#' @param ... key-value paries of search fields. Use `sk_get_search_fields()`
 #' to see a list of fields and their potential values.
 #' @param return_tibbles Whether to return the data (if TRUE) or only download the files (if FALSE).
 #' @param keep_files Whether to write the XLS files locally.
@@ -65,132 +155,46 @@ sk_get_search_fields <- function(search_form = NULL) {
 #' To avoid blitzing the data provider's server with many heavy requests:
 #'
 #' 1. If you need more tables based on the same search, pass it in one call,
-#' using the `tables` argument. This means that only one initial search is peformed.
+#' using the `tables` argument. This means that only one initial search is
+#' peformed.
 #' 1. Only ask for the tables you need.
-#' 1. If you need a subset of the data, use the `fields` argument
-#' 1. If you need multiple subsets of the data, try to do that via the `fields`
-#' argument too, though that may not always be possible.
-#' 1. If you are downloading a large dump and reusing it in a pipeline, keep the
-#' downloaded XLS files (or your own export) locally (setting `keep_files` to TRUE),
-#' use caching and avoid calling this function repeatedly (ideally make any reruns
-#' conditional on the age of the stored export).
+#' 1. If you need a subset of the
+#' data, use the `fields` argument
+#' 1. If you need multiple subsets of the data,
+#' try to do that via the `fields` argument too, though that may not always be
+#' possible.
+#' 1. If you are downloading a large dump and reusing it in a
+#' pipeline, keep the downloaded XLS files (or your own export) locally (setting
+#' `keep_files` to TRUE), use caching and avoid calling this function repeatedly
+#' (ideally make any reruns conditional on the age of the stored export).
 #'
-#' @return if return_tibbles is TRUE, a named list of [tibbles][tibble::tibble-package],
-#' with a tibble for each table in `tables` with the corresponding name; otherwise a character vector of file paths.
-#' Note that the resulting XLS files are in fact HTML files and you are best off
-#' loading them using `sk_load_directory()` and tidying with `sk_process_directory`,
-#' though they can be opened in Excel too.
+#' @return if return_tibbles is TRUE, a named list of
+#'   [tibbles][tibble::tibble-package], with a tibble for each table in `tables`
+#'   with the corresponding name; otherwise a character vector of file paths.
+#'   Note that the resulting XLS files are in fact HTML files and you are best
+#'   off loading them using `sk_load_directory()` and tidying with
+#'   `sk_process_directory`, though they can be opened in Excel too.
 #'
 #' @examples
-#' sk_get_directory("addresses", fields = c(uzemi = "CZ010"), return_tibbles = T, write_files = T)
+#' sk_get_directory("addresses", uzemi = "CZ010", return_tibbles = T, write_files = T)
+#' @export
 sk_get_directory <- function(tables = NULL,
-                           fields = NULL,
-                           return_tibbles = FALSE,
-                           keep_files = TRUE) {
+                             ...,
+                             return_tibbles = FALSE,
+                             keep_files = TRUE) {
 
-  if(is.null(tables)) {
-    tabs <- c("addresses", "schools", "locations", "specialisations")
-  } else {
-    tabs <- tables
-  }
+  responses <- sk_get_directory_responses(tables = tables, ...)
 
-  search_page <- sk_get_search_page()
-  search_form <- sk_get_search_form(search_page)
-
-  fields_to_make_default <- c("uzemi","zrizovatel", "organ","typ", "jazs","delka",
-                              "forma","jazob","skupobor","kmobor","obor")
-
-  search_form_fields <- sk_get_search_fields(search_form)
-
-  field_names <- names(search_form_fields)
-
-  search_form_fields_with_defaults <- list()
-  for (form_field in search_form_fields) {
-    if(form_field[['name']] %in% fields_to_make_default) form_field['value'] <- "NIC"
-    search_form_fields_with_defaults <- append(search_form_fields_with_defaults,
-                                               list(form_field))
-  }
-
-  names(search_form_fields_with_defaults) <- field_names
-
-  if(!is.null(fields)) {
-    fields_for_userupdate <- names(fields)
-    search_form_fields_userupdated <- list()
-    for (form_field in search_form_fields_with_defaults) {
-      if(form_field[['name']] %in% fields_for_userupdate) {
-        form_field['value'] <- fields[form_field[['name']]]
-      }
-      search_form_fields_userupdated <- append(search_form_fields_userupdated,
-                                                 list(form_field))
-    }
-    search_form_fields_final <- search_form_fields_userupdated
-  } else {
-    search_form_fields_final <- search_form_fields_with_defaults
-  }
-
-  names(search_form_fields_final) <- field_names
-  search_form_filled <- search_form
-  search_form_filled$fields <- search_form_fields_final
-
-  results_page <- rvest::submit_form(search_page, search_form_filled, "XX")
-
-  results_forms <- results_page %>% xml2::read_html() %>%
-    rvest::html_nodes("form") %>%
-    rvest::html_form()
-
-  export_page <- rvest::submit_form(results_page, results_forms[[2]], "EX")
-
-  export_forms <- rvest::html_form(export_page)
-
-  print("downloading data")
-  responses <- list()
-  if("addresses" %in% tabs) {
-    responses <- append(responses, list(rvest::submit_form(export_page, export_forms[[1]], "EX")))
-    names(responses) <- c(names(responses), "addresses")
-  }
-  if("schools" %in% tabs) {
-    responses <- append(responses, list(rvest::submit_form(export_page, export_forms[[2]], "EXX")))
-    names(responses) <- c(names(responses), "schools")
-  }
-  if("locations" %in% tabs) {
-    responses <- append(responses, list(rvest::submit_form(export_page, export_forms[[3]], "EXM")))
-    names(responses) <- c(names(responses), "locations")
-    }
-  if("specialisations" %in% tabs) {
-    responses <- append(responses, list(rvest::submit_form(export_page, export_forms[[4]], "EXO")))
-    names(responses) <- c(names(responses), "specialisations")
-    }
-
-    print("writing files")
   paths <- purrr::map_chr(responses, response_to_quasixls, !keep_files)
   names(paths) <- names(responses)
 
   if(return_tibbles) {
-    print("loading data")
     rslt <- purrr::map(paths, sk_load_stistko)
   } else {
     rslt <- paths
   }
 
   return(rslt)
-
-}
-
-response_to_quasixls <- function(response, tempfile = TRUE) {
-
-  if(tempfile) {
-    path <- tempfile(fileext = ".xls")
-  } else {
-    attachment_string <- response$response$headers$`content-disposition`
-    path <- regmatches(attachment_string,
-                       regexpr("(?<=filename\\=)(\\w*.xls$)",
-                               attachment_string, perl = T))
-  }
-
-  r3_c_bin <- httr::content(response$response)
-  writeBin(r3_c_bin, path)
-
-  return(path)
 }
 
 sk_load_stistko <- function(path) {
