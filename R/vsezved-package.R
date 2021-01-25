7deea320331521c188714cf3191f0ea8773a078a
Petr Bouchal <pbouchal@gmail.com>
2021-01-25 12:21:26 +0100

    rename package

--- R/vsezved-package.R
+++ R/vsezved-package.R
@@ -4,14 +4,10 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
-## usethis namespace: end
-NULL
-
-## usethis namespace: start
 #' @importFrom tibble tibble
 ## usethis namespace: end
 NULL
 
 #' @importFrom dplyr select filter mutate rename
-#' @importFrom usethis ui_info ui_value ui_field ui_done ui_code_block ui_line ui_stop ui_path ui_oops ui_todo ui_warn ui_yeah
+#' @importFrom usethis ui_info ui_value ui_field ui_code ui_done ui_code_block ui_line ui_stop ui_path ui_oops ui_todo ui_warn ui_yeah
 NULL
