1e45ff1fbfdee1003b486e9ebd25101c0111d0bd
Petr Bouchal <pbouchal@gmail.com>
2024-07-12 11:34:58 +0200

    fix imports warnings

--- R/vsezved-package.R
+++ R/vsezved-package.R
@@ -4,10 +4,12 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
+#' @importFrom lifecycle deprecated
 #' @importFrom tibble tibble
 ## usethis namespace: end
 NULL
 
 #' @importFrom dplyr select filter mutate rename starts_with matches ends_with
+#' @importFrom prettyunits pretty_bytes
 #' @importFrom usethis ui_info ui_value ui_field ui_code ui_done ui_code_block ui_line ui_stop ui_path ui_oops ui_todo ui_warn ui_yeah
 NULL
